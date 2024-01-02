3051ae8beac7937daf7fac182d57542940ff2999
Kristen Daya Lee <clare0339@gmail.com>
2024-01-02 09:55:23 +0900

    Java Happy new year

---
+++ src/variable_practice/Var2.java
@@ -0,0 +1,12 @@
+package variable_practice;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; //변수 선수 숫자를 담는 그릇~
+        a= 10; //변수 초기화
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
